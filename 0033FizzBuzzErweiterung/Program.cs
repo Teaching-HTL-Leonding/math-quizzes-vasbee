0f62e4b28afbceca294128a31031d4cb483b3352
vasbee <114105197+vasbee@users.noreply.github.com>
2022-12-01 11:56:51 +0100

    Add files via upload

---
+++ 0033FizzBuzzErweiterung/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Largest number");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Write("Fizz ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Buzz ");
+int b = int.Parse(Console.ReadLine()!);
+
+
+for (int i = 1; i < n; i++)
+{
+    if (i % n == 0 && i % 3 == 0)
+    {
+        Console.Write("FizzBuzz ");
+    }
+
+    else if (i % a == 0)
+    {
+        Console.Write("Fizz ");
+    }
+
+    else if (i % b == 0)
+    {
+        Console.Write("Buzz ");
+    }
+    else if (i != n && i != n)
+    {
+        Console.Write($"{i} ");
+    }
+}
